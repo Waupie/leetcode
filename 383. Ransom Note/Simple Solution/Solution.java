29ef1c96b3a2ce73d2da09efbf0fe6ca35d8556d
Waupie <waup.max@gmail.com>
2025-04-07 09:57:29 +0200

    Finished simple solution for '383. Ransom Note'.

---
+++ 383. Ransom Note/Simple Solution/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Author:      Maximilian Holm
+ * Date:        07/04/2025
+ * Description: This method checks if the given ransomNote can be constructed 
+ *              using characters from the given magazine. It does so by 
+ *              verifying if the magazine contains the ransomNote as a whole. 
+ *              The method returns true if it can be constructed, otherwise false.
+ */
+
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (magazine.contains(ransomNote))
+            return true;
+        else
+            return false;
+    }
+}
